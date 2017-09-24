a38927c67b678cf3f36bffdeb1ee422dd48e0f18
Johan Esteves <jsesteves91@gmail.com>
2017-09-24 19:30:49 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,12 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT serires.title FROM series
+  JOIN characters ON series.id = characters.series_id
+  GROUP BY series.title
+  HAVING characters.species = 'human';
+
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
