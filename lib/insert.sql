2cafb9591d1d630a4a671208e469e0c18479f6f7
Johan Esteves <jsesteves91@gmail.com>
2017-09-24 18:59:41 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -25,4 +25,22 @@ INSERT INTO characters(name,motto, species, author_id, series_id) VALUES ("name6
 INSERT INTO characters(name,motto, species, author_id, series_id) VALUES ("name7","motto7", "species7", 2, 2);
 INSERT INTO characters(name,motto, species, author_id, series_id) VALUES ("name8","motto8", "species8", 2, 2);
 
-INSERT INTO character_books (character_id, book_id) VALUES ()
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 3);
+INSERT INTO character_books (character_id, book_id) VALUES (2, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (2, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (2, 3);
+
+INSERT INTO character_books (character_id, book_id) VALUES (3, 3);
+INSERT INTO character_books (character_id, book_id) VALUES (4, 3);
+
+INSERT INTO character_books (character_id, book_id) VALUES (5, 4);
+INSERT INTO character_books (character_id, book_id) VALUES (5, 5);
+INSERT INTO character_books (character_id, book_id) VALUES (5, 6);
+INSERT INTO character_books (character_id, book_id) VALUES (6, 4);
+INSERT INTO character_books (character_id, book_id) VALUES (6, 5);
+INSERT INTO character_books (character_id, book_id) VALUES (6, 6);
+
+INSERT INTO character_books (character_id, book_id) VALUES (7, 5);
+INSERT INTO character_books (character_id, book_id) VALUES (8, 5);
