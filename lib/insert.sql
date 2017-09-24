cf9daa6202d8c58d4fdf53b3da5ec9f05b48aaad
Johan Esteves <jsesteves91@gmail.com>
2017-09-24 18:55:26 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
-INSERT INTO series(title, author_id, series_id) VALUES ("series1", 1, 2);
-INSERT INTO series(title, author_id, series_id) VALUES ("series2", 2, 1);
+INSERT INTO series(title, author_id, subgenre_id) VALUES ("series1", 1, 2);
+INSERT INTO series(title, author_id, subgenre_id) VALUES ("series2", 2, 1);
 
 INSERT INTO subgenres(name) VALUES ("subgenre1");
 INSERT INTO subgenres(name) VALUES ("subgenre2");
@@ -7,8 +7,22 @@ INSERT INTO subgenres(name) VALUES ("subgenre2");
 INSERT INTO authors(name) VALUES ("author1");
 INSERT INTO authors(name) VALUES ("author2");
 
-INSERT INTO books(title, author_id, series_id) VALUES ("book1", 1999, 1)
-INSERT INTO books(title, author_id, series_id) VALUES ("book2", 2000, 1)
-INSERT INTO books(title, author_id, series_id) VALUES ("book3", 2001, 2)
+INSERT INTO books(title, author_id, series_id) VALUES ("book1", 1999, 1, 1);
+INSERT INTO books(title, author_id, series_id) VALUES ("book2", 2000, 1, 1);
+INSERT INTO books(title, author_id, series_id) VALUES ("book3", 2001, 1, 1);
 
-INSERT INTO series(title, author_id, series_id) VALUES ("series1", 1, 2)
+INSERT INTO books(title, author_id, series_id) VALUES ("book4", 1999, 2, 2);
+INSERT INTO books(title, author_id, series_id) VALUES ("book5", 2000, 2, 2);
+INSERT INTO books(title, author_id, series_id) VALUES ("book6", 2001, 2, 2);
+
+INSERT INTO characters(name,motto, species, author_id, series_id) VALUES ("name1","motto1", "species1", 1, 1);
+INSERT INTO characters(name,motto, species, author_id, series_id) VALUES ("name2","motto2", "species2", 1, 1);
+INSERT INTO characters(name,motto, species, author_id, series_id) VALUES ("name3","motto3", "species3", 1, 1);
+INSERT INTO characters(name,motto, species, author_id, series_id) VALUES ("name4","motto4", "species4", 1, 1);
+
+INSERT INTO characters(name,motto, species, author_id, series_id) VALUES ("name5","motto5", "species5", 2, 2);
+INSERT INTO characters(name,motto, species, author_id, series_id) VALUES ("name6","motto6", "species6", 2, 2);
+INSERT INTO characters(name,motto, species, author_id, series_id) VALUES ("name7","motto7", "species7", 2, 2);
+INSERT INTO characters(name,motto, species, author_id, series_id) VALUES ("name8","motto8", "species8", 2, 2);
+
+INSERT INTO character_books (character_id, book_id) VALUES ()
