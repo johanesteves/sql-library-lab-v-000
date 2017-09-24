bcd3ead4fe738f6f59486fb6d882d14f1b2ff744
Johan Esteves <jsesteves91@gmail.com>
2017-09-24 19:24:18 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species) FROM characters
+  "SELECT characters.species, MAX(characters.species) FROM characters
    GROUP BY characters.species
    HAVING MAX(COUNT(characters.species));
 
