c7c8dcdffa153f0d96841fb518c3f1f50e63655c
Johan Esteves <jsesteves91@gmail.com>
2017-09-24 19:21:14 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,10 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species FROM characters
+   GROUP BY characters.species;
+
+  "
 end
 
 def select_name_and_series_subgenres_of_authors
